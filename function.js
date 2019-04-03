6c2c5c23d071a12f6b767d7ded41469ce9ed3364
Keith Harris <keithharris.student@careerdevs.com>
2019-04-03 02:33:40 +0000

    First commit

---
+++ function.js
@@ -0,0 +1,7 @@
+
+
+function addvalues (x,y,z){  // the function takes in 3 arguments
+    console.log(x + y + z);  // the function returns the sum of all 3 arguments
+}
+
+addvalues(12,3,4);   // the function is called and arguments are passed into it
